663285a373117fd5dc7beddadb8d7d37e23717f3
ranje <ranjeetkumarbhutul@gmail.com>
2022-10-04 21:54:54 +0530

    Method Update

--- JavaCodeBase/src/CollectionFramework/MethodOverloadingInCollection.java
+++ JavaCodeBase/src/CollectionFramework/MethodOverloadingInCollection.java
@@ -2,8 +2,17 @@ package CollectionFramework;
 
 public class MethodOverloadingInCollection {
 	public static void main(String[]args) {
-		
-		
 	}
+		
+		public void ranjeet() {
+			int i =20;
+			int j =30;
+			int sum = i+j;
+			System.out.println(sum);
+			
+			
+		}
+		
+	
 
 }
